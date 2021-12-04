c2d9f701d5f5ff476d04d649845e1fab17480079
vbion <73699379+vbion@users.noreply.github.com>
2021-12-04 21:34:57 +0100

    Update drop-probability-calculator.py

--- other-versions/drop-probability-calculator.py
+++ other-versions/drop-probability-calculator.py
@@ -1,5 +1,5 @@
 # The version where I don't optimize the item check. Easier to follow what happens in the code.
-# The runtime slower than the main version, but this one can serve as a checker if needed.
+# The runtime is slower than the main version, but this one can serve as a checker if needed.
 # Each roll goes linearly through the list of desired items until it fits or the list runs out.
 
 def generate_thresholds(desired_items):
